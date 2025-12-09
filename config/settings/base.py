7c8d5a9b92750c8a21ca2bb17ae244b53ca87e65
jingyulee <max322318@gmail.com>
2025-12-09 11:27:35 +0900

    REFACTOR :: allowed host를 모두 허용으로 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost"])
+ALLOWED_HOSTS = ["*"]
 
 
 # -----------------------------
